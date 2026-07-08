d1c7568511e06f7a809eb0ecf9bb444c98d418d0
Dennis Mulder <dnns.mldr@gmail.com>
2026-07-08 09:18:40 +0200

    Werkende roomtpot

---
+++ app/scrapers/roompot.py
@@ -0,0 +1,36 @@
+from app.models.deal import Deal
+from app.scrapers.base import BaseScraper
+
+
+class LandalScraper(BaseScraper):
+
+    def __init__(self, config):
+        self.config = config
+
+    def scrape(self):
+
+        adults = self.config["family"]["adults"]
+        children = self.config["family"]["children"]
+
+        departure_dates = self.config["trip"]["departure_dates"]
+        nights = self.config["trip"]["nights"]
+
+        # Voor nu alleen even loggen zodat we zien dat de YAML wordt gebruikt.
+        print(
+            f"Landal: {adults} volwassenen, "
+            f"{len(children)} kinderen, "
+            f"{departure_dates}, "
+            f"{nights} nachten"
+        )
+
+        deals = [
+            Deal(
+                source="Landal",
+                title="Park van Landal",
+                location="Ergens",
+                price=688,
+                url="https://www.landal.nl/",
+            )
+        ]
+
+        return deals
