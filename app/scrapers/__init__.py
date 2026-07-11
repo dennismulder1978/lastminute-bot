35b766f3a558bc8935ad4ea9770e3c9a54160f4f
Dennis Mulder grote PC <dnns.mldr@gmail.com>
2026-07-11 23:24:37 +0200

    opschoenne

--- app/scrapers/__init__.py
+++ app/scrapers/__init__.py
@@ -3,9 +3,6 @@ from app.scrapers.landal import LandalScraper
 from app.scrapers.roompot import RoompotScraper
 from app.scrapers.roan import RoanScraper
 from app.scrapers.eurocamps import EurocampScraper
-# from app.scrapers.allcamps import AllcampsScraper
-# from app.scrapers.vacanceselect import VacanceSelectScraper
-# from app.scrapers.luxcamp import LuxCampScraper
 from app.scrapers.twenty_scrapers import AllcampsScraper, VacanceSelectScraper, LuxCampScraper, FriferieScraper, DanskBilferieScraper
 
 
